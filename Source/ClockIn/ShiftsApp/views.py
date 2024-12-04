b3c52c0c4599ea8da413163f52d27496d77a2e98
grzegorz1q <grzegorbaryla@gmail.com>
2024-12-04 14:50:55 +0100

    Moved views to appropriate applications

--- Source/ClockIn/ShiftsApp/views.py
+++ Source/ClockIn/ShiftsApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def shifts_view(request):
+    return render(request, 'shiftsView.html')
+
+def manage_shifts_view(request):
+    return render(request, 'manageShiftView.html')
