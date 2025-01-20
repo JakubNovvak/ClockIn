925836787538795545f9110cb60ab763dec7cd2a
Jakub Nowak <109487772+JakubNovvak@users.noreply.github.com>
2025-01-20 13:32:19 +0100

    Implement POST request handling of the adminMangeUsers view, and fix issues related to proper handling and displaying decimal values in the forms

--- Source/ClockIn/UsersApp/views.py
+++ Source/ClockIn/UsersApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login , logout
 from django.contrib import messages
 from UsersApp.models import User, Department
@@ -84,10 +84,32 @@ def admin_manage_users(request):
         'departments': []
     }
 
+    if request.method == "POST":
+        user_to_update = get_object_or_404(User, id=request.POST.get("user_id"))
+
+        user_to_update.first_name = request.POST.get("name")
+        user_to_update.last_name = request.POST.get("surname")
+        user_to_update.username = request.POST.get("username")
+        user_to_update.email = request.POST.get("email")
+        user_to_update.department_id = request.POST.get("department_id")
+        user_to_update.hourly_rate = float(request.POST.get("hourlyRate"))
+
+        user_to_update.is_active = bool(request.POST.get("is_active"))
+
+        user_to_update.save()
+
+        print('Aktualizacja Usera')
+
     context['users'] = User.objects.filter(is_superuser=False)
     context['departments'] = Department.objects.all()
 
-    
+    for user in context['users']:
+        if user.hourly_rate == None:
+            user.hourly_rate = 0.1
+        else:
+            user.hourly_rate = float(user.hourly_rate)
+            print(user.hourly_rate)
+
 
     return render(request, 'adminManageUsers.html', context)
 
